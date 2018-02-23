b61b600507cab25eb47995f4a615d1972121ba47
Neil MacDougall <neil_macdougall@hotmail.com>
2018-02-23 16:16:25 +0000

    Pin commits

--- get-depends.sh
+++ get-depends.sh
@@ -2,7 +2,12 @@
 
 echo "Getting repos"
 
-BRANCH=release-2.0
+REPOS=(
+  "automation:079c01bc68dc30275e51b909a38269cb03014b02"
+  "salt:7c41eedc100f7e3bf4b0a208840d5ecf37f2a195"
+  "velum:5664b456ef41b5dc5f9fa61e0ec7714ab39a4d9a"
+  "caasp-container-manifests:dc67c61146189477543707b327bf533612bbeaab"
+)
 
 ORIG_DOMAIN="devenv.caasp.suse.net"
 NEW_DOMAIN="devenv.capbristol.com"
@@ -11,8 +16,8 @@ NEW_DOMAIN="devenv.capbristol.com"
 function cloneOrUpdate {
 
   GIT_REPO="https://github.com/kubic-project/$1.git"
-
-  echo "Git repo: ${GIT_REPO}"
+  BRANCH=$2
+  echo "Git repo: ${GIT_REPO}, commit: ${BRANCH}"
 
   if [ ! -d ./$1 ]; then
     echo "Cloning: ${GIT_REPO}"
@@ -23,6 +28,7 @@ function cloneOrUpdate {
     git fetch
     git reset --hard HEAD
     git clean -f
+    git checkout ${BRANCH}
     git rebase
     popd
   fi	
@@ -39,11 +45,17 @@ if [ -d ./automation/downloads ]; then
   cp -R ./automation/downloads .downloads
 fi
 
+for repo in "${REPOS[@]}" ; do
+    REPO=${repo%%:*}
+    COMMIT=${repo#*:}
+    cloneOrUpdate $REPO $COMMIT
+done
+
 # Get caasp automation repos
-cloneOrUpdate automation
-cloneOrUpdate salt
-cloneOrUpdate velum
-cloneOrUpdate caasp-container-manifests
+#cloneOrUpdate automation
+#cloneOrUpdate salt
+#cloneOrUpdate velum
+#cloneOrUpdate caasp-container-manifests
 
 # Patch files for our names
 
